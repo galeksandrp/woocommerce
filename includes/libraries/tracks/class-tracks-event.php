41810f629cd6f56e80d3481f9d6275d5ab05b423
Paul Sealock <psealock@gmail.com>
2019-02-04 17:57:59 +1300

    lint client-events

--- includes/libraries/tracks/class-tracks-event.php
+++ includes/libraries/tracks/class-tracks-event.php
@@ -43,7 +43,7 @@ class Tracks_Event {
 	const PROP_NAME_REGEX  = '/^[a-z_][a-z0-9_]*$/';
 	public $error;
 
-	function __construct( $event ) {
+	public function __construct( $event ) {
 		$_event = self::validate_and_sanitize( $event );
 		if ( is_wp_error( $_event ) ) {
 			$this->error = $_event;
@@ -55,25 +55,30 @@ class Tracks_Event {
 		}
 	}
 
-	function record() {
+	/**
+	 * Record Tracks event
+	 *
+	 * @return mixed Result of the event.
+	 */
+	public function record() {
 		return Tracks_Client::record_event( $this );
 	}
 
 	/**
 	 * Annotate the event with all relevant info.
 	 *
-	 * @param  mixed $event Object or (flat) array
+	 * @param  mixed $event Object or (flat) array.
 	 * @return mixed        The transformed event array or WP_Error on failure.
 	 */
-	static function validate_and_sanitize( $event ) {
+	public static function validate_and_sanitize( $event ) {
 		$event = (object) $event;
 
-		// Required
+		// Required.
 		if ( ! $event->_en ) {
 			return new WP_Error( 'invalid_event', 'A valid event must be specified via `_en`', 400 );
 		}
 
-		// delete non-routable addresses otherwise geoip will discard the record entirely
+		// delete non-routable addresses otherwise geoip will discard the record entirely.
 		if ( property_exists( $event, '_via_ip' ) && preg_match( '/^192\.168|^10\./', $event->_via_ip ) ) {
 			unset( $event->_via_ip );
 		}
@@ -99,7 +104,7 @@ class Tracks_Event {
 	 *
 	 * @return string A pixel URL or empty string ('') if there were invalid args.
 	 */
-	function build_pixel_url() {
+	public function build_pixel_url() {
 		if ( $this->error ) {
 			return '';
 		}
@@ -119,15 +124,32 @@ class Tracks_Event {
 		return Tracks_Client::PIXEL . '?' . http_build_query( $validated );
 	}
 
-	static function event_name_is_valid( $name ) {
+	/**
+	 * Check if event name is valid.
+	 *
+	 * @param string $name Event name.
+	 * @return false|int
+	 */
+	public static function event_name_is_valid( $name ) {
 		return preg_match( self::EVENT_NAME_REGEX, $name );
 	}
 
-	static function prop_name_is_valid( $name ) {
+	/**
+	 * Check if a property name is valid.
+	 *
+	 * @param string $name Event property.
+	 * @return false|int
+	 */
+	public static function prop_name_is_valid( $name ) {
 		return preg_match( self::PROP_NAME_REGEX, $name );
 	}
 
-	static function scrutinize_event_names( $event ) {
+	/**
+	 * Check event names
+	 *
+	 * @param object $event An event object.
+	 */
+	public static function scrutinize_event_names( $event ) {
 		if ( ! self::event_name_is_valid( $event->_en ) ) {
 			return;
 		}
@@ -138,7 +160,7 @@ class Tracks_Event {
 		);
 
 		foreach ( array_keys( (array) $event ) as $key ) {
-			if ( in_array( $key, $whitelisted_key_names ) ) {
+			if ( in_array( $key, $whitelisted_key_names, true ) ) {
 				continue;
 			}
 			if ( ! self::prop_name_is_valid( $key ) ) {
